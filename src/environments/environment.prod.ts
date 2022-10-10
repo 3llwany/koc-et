a4cee9187c67a98f656d2c0993d35ded091a8369
Khaled-Ebrahem <thepharaoh42@yahoo.com>
2022-10-10 10:55:50 +0200

    st

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   // apiURL: "http://localhost:18947/api/",
   // apiURL: "https://testmozakretyapi.cloudiax.com/api/", //Test;
-  apiURL: "https://mozakretyapi.cloudiax.com/api/", //Live;
+  //apiURL: "https://mozakretyapi.cloudiax.com/api/", //Live;
+  apiURL: "https://ogsetapi.cloudiax.com/api/", //Live;
 };
